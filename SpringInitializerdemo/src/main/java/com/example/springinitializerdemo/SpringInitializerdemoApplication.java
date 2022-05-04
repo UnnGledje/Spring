a0fb69f7a110e8f12f6f93ebecc8ba7f2564a307
UnnGledje <92151619+UnnGledje@users.noreply.github.com>
2022-05-04 13:23:10 +0200

    Add files via upload

---
+++ SpringInitializerdemo/src/main/java/com/example/springinitializerdemo/SpringInitializerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinitializerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInitializerdemoApplication.class, args);
+    }
+
+}
